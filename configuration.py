9cb39bb6a6f440d34bf4ada6fb2f91ca8ae71b1c
Dianny <dianny.97@hotmail.com>
2024-07-05 19:38:56 -0300

    corrección

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-c14662db-034e-4008-ada0-01d7b8962d88.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-93749433-b147-4035-9050-8635633c624c.containerhub.tripleten-services.com"
 
 CREATE_USER_URL = "/api/v1/users"
 CREATE_KIT_URL = "/api/v1/kits"
